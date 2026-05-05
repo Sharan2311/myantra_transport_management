9fcfdfc1475406180dbf21c0a7ecb9efdbc0cb03
sharan2311 <sharan2311@gmail.com>
2026-05-05 19:02:37 +0530

    Admin Supabase URL

--- src/client.config.js
+++ src/client.config.js
@@ -5,8 +5,8 @@
 
 const CLIENT_CONFIG = {
   clientId:            "1788c828-9232-4247-bcaa-c9dd92ce1bc6",
-  adminSupabaseUrl:    "https://dmccaowxwfsvzokawxoh.supabase.co",
-  adminSupabaseAnonKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtY2Nhb3d4d2Zzdnpva2F3eG9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2Nzk5NDcsImV4cCI6MjA4ODI1NTk0N30.CoEmk9kpzU6KdOMtc3LfODK_KRXguyh9afxnmxmhNs0",
+  adminSupabaseUrl:    "https://rthxunzrwwcrarinhqnz.supabase.co",
+  adminSupabaseAnonKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0aHh1bnpyd3djcmFyaW5ocW56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MTk4MjgsImV4cCI6MjA5MzQ5NTgyOH0.cM9Ya4Tfki7HAzx-Ny1cWmi82NJq52ofrQy9S7FT-xQ",
 };
 
 export default CLIENT_CONFIG;
